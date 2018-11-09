4575a031cd45e6a94391c4aed6c6606027aa0c0f
Mrinal Sinha <themrinalsinha@gmail.com>
2018-11-09 23:57:24 +0530

    ready

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from qr_code.qrcode.utils import QRCodeOptions
 
 def index(request):
     if request.POST:
-        config     = QRCodeOptions(size='L', version=1, border=6, error_correction='L')
+        config     = QRCodeOptions(size='m', version=1, border=2, error_correction='L', image_format="png")
         user_input = request.POST.get('user_input')
         return render(request, 'base.html', {'popup':True, 'config':config, 'user_input':user_input})
     return render(request, 'base.html', {})
